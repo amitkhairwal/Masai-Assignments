6658eff6744e0af83284560cc74751b2d3590765
Amit <kamit.veya.in>
2025-09-03 17:55:02 +0530

    Question 11 html

---
+++ Prototypical Inheritance - I/Question10.js
@@ -0,0 +1,36 @@
+// export default function Question10(){
+// //     Create a constructor function for Car that takes the following parameters:
+
+// //     make: the make of the car (e.g., Toyota)
+// //     model: the model of the car (e.g., Corolla)
+// //     year: the year of the car (e.g., 2020)
+// //     isAvailable: a boolean indicating if the car is available for rent (default is true)
+
+// // Create a constructor function for Customer that takes the following parameters:
+
+// //     name: the name of the customer
+// //     rentedCars: an array to store the cars rented by the customer (default is an empty array)
+
+// // Add a method to the Customer prototype to allow customers to rent a car. This method should:
+
+// //     Take a Car object as an argument.
+// //     Check if the car is available.
+// //     If the car is available, mark it as not available and add the car to the customer's rentedCars array.
+// //     If the car is not available, log a message saying the car is already rented.
+
+
+//     function Car(make,model, year,isAvailable){
+//         this.make=make,
+//         this.model=model,
+//         this.year=year
+//         this.isAvailable=isAvailable
+//     }
+
+//     function Customer(name,rentedCars){
+//         this.name=name,
+//         this.rentedCars=rentedCars
+//     }
+
+//     Customer.prototype.
+
+// }
